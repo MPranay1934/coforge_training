ec20a05fa79d961b7bcf0aa3abd9f980c615fa83
Sreejamatri <matrisreeja@gmail.com>
2026-07-03 17:02:25 +0530

    Added day 3

---
+++ Assignment/BookApp.java
@@ -0,0 +1,13 @@
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		Book book1 = new Book (101,"Enkates Book","Enkates",590);
+		book1.display();
+		
+		Book book2 = new Book(102,"Gundu Book","Gundu",450);
+		book2.display();
+
+	}
+
+}
